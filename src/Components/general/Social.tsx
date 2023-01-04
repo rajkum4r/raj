ed59e90f7887e10a0fef4abd48191f5d5f6662dc
rajkum4r <121956595+rajkum4r@users.noreply.github.com>
2023-01-04 11:21:24 -0500

    Update Social.tsx

--- src/Components/general/Social.tsx
+++ src/Components/general/Social.tsx
@@ -7,7 +7,7 @@ const Social = () => {
             <ul>
                 <li><a href={'mailto:rkkumarraja91@gmail.com'} className="envelope"><i className="fa fa-envelope"></i></a></li>
                 <li><a target="_blank" href="https://www.linkedin.com/in/raj-kumar-790968260/" className="linkedin" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a></li>
-                {/* <li><a target="_blank" href="https://github.com/Sach7n" className="github" rel="noopener noreferrer"><i className="fa fa-github"></i></a></li> */}
+                {/* <li><a target="_blank" href="https://github.com" className="github" rel="noopener noreferrer"><i className="fa fa-github"></i></a></li> */}
             </ul>
         </div>
     );
